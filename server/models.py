3518017707543459cfd0d76bc506608ad6c1a352
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-09-30 21:23:20 -0400

    implements user model

--- server/models.py
+++ server/models.py
@@ -1,6 +1,18 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    name = db.Column(db.String(255), nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    height = db.Column(db.Float, nullable=False)
+
+    
