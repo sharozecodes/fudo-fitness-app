3983f41668a3e5794c59b02cef14dcf8d3a204d6
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-10-02 14:01:34 -0400

    generates user seed.py

--- server/seed.py
+++ server/seed.py
@@ -16,3 +16,22 @@ if __name__ == '__main__':
         print("Deleting all records...")
         Recipe.query.delete()
         User.query.delete()
+        Workout.query.delete()
+        WorkoutPreference.query.delete()
+        RecipePreference.query.delete()
+
+        print("Creating new records...")
+        for i in range(20):
+            
+            username = fake.user_name()
+            name = fake.name()
+            date_of_birth = fake.date_of_birth(minimum_age=18, maximum_age=70)
+            weight = randint(80,500)
+            height = randint(60, 272)
+
+            user = User(username=username, name=name, date_of_birth=date_of_birth, weight=weight, height=height)
+            user.password_hash = user.username + 'password'
+            db.session.add(user)
+
+        db.session.commit()
+
